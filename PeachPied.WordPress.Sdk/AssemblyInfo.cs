d6f9d6546c0fc4a445622c7a683999a31e275f31
Jakub Míšek <jmisek@gmail.com>
2019-03-27 14:22:15 +0100

    typos and startup folder

--- PeachPied.WordPress.Sdk/AssemblyInfo.cs
+++ PeachPied.WordPress.Sdk/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using Pchp.Core;
 
 // denotates the assembly contains PeachPie extensions:
-// global constants and gobal functions
+// global constants and global functions
 // https://docs.peachpie.io/api/Libraries-Architecture/
 [assembly: PhpExtension]
