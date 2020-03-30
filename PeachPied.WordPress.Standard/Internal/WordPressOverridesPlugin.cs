cc3973d9d4ce8f8cb47f0252c0268926be93141d
Jakub Míšek <jmisek@gmail.com>
2020-03-30 21:24:44 +0200

    dashboard informational box

--- PeachPied.WordPress.Standard/Internal/WordPressOverridesPlugin.cs
+++ PeachPied.WordPress.Standard/Internal/WordPressOverridesPlugin.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Composition;
 using System.IO;
 using System.Reflection;
+using System.Runtime.Versioning;
 using System.Text;
 
 namespace PeachPied.WordPress.Standard.Internal
@@ -19,17 +20,32 @@ namespace PeachPied.WordPress.Standard.Internal
 
     sealed class WordPressOverridesPlugin : IWpPlugin
     {
+        public static string WpDotNetUrl = "https://github.com/iolevel/wpdotnet-sdk";
+
         public static readonly string InformationalVersion = typeof(WordPressOverridesPlugin).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
 
         static void DashboardRightNow(TextWriter output)
         {
-            output.Write(
-                "<ul>" +
-                "<li><img src='{0}' style='width:76px;margin:0 auto;display:block;'/></li>" +
-                "<li>{1}</li>" +
-                "</ul>",
-                "https://raw.githubusercontent.com/peachpiecompiler/peachpie/master/docs/logos/round-orange-196x196.png",
-                "<b>Hello from .NET!</b><br/>The site is powered by .NET Core instead of PHP, compiled entirely to MSIL bytecode using <a href='https://www.peachpie.io/' target='_blank'>PeachPie</a>.");
+            var frameworkVersion =
+                System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription ??
+                Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
+            var process = System.Diagnostics.Process.GetCurrentProcess();
+            
+            output.Write($@"
+<table style=""border:0;width:100%;""><tbody>
+<tr>
+    <td><img src=""https://raw.githubusercontent.com/peachpiecompiler/peachpie/master/docs/logos/round-orange-196x196.png"" style=""width:76px;margin:8px;display:inline;""></td>
+    <td>
+        <b>Your WordPress is running on .NET</b>
+        <div>
+            <b>Framework:</b> {frameworkVersion}<br/>
+            <b title=""Memory allocated by the whole process."">Memory usage:</b> {process.WorkingSet64/1024/1024} MB<br/>
+            <b title=""CPU time consumed by the whole process."">CPU usage:</b> {process.TotalProcessorTime:c}<br/>
+            <b>Project URL:</b> <a href=""{WpDotNetUrl}"" target=""_blank"">wpdotnet-sdk</a><br/>
+        </div>
+    </td>
+</tr>
+</tbody></table>");
         }
 
         static readonly string GeneratorHtml = "<meta name=\"generator\" content=\"WpDotNet (PeachPie) " + InformationalVersion + " \" />";
