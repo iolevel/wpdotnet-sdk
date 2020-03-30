39619938fde4b8f72668aa2a3eb330634a6958fa
Jakub Míšek <jmisek@gmail.com>
2020-03-30 16:25:43 +0200

    SDK: Peachpied.WordPress.Build.Plugin
    - common build sdk for plugins and themes
    - simplified project structure

---
+++ Peachpied.WordPress.Build.Plugin/README.md
@@ -0,0 +1,2 @@
+# WordPress.NET.Plugin.Sdk
+
---
+++ Peachpied.WordPress.Build.Plugin/WordPressTask.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace Peachpied.WordPress.Build.Plugin
+{
+    public class PluginTask : Task
+    {
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}
