6577844737c49f19bc94714697ab9a5d3d7f4c76
Jakub Míšek <jmisek@gmail.com>
2021-04-03 18:03:32 +0200

    hotplug class

--- plugins/PeachPied.WordPress.HotPlug/Provider.cs
+++ plugins/PeachPied.WordPress.HotPlug/Provider.cs
@@ -6,14 +6,14 @@ using System.IO;
 using Pchp.Core;
 using PeachPied.WordPress.Standard;
 
-namespace Peachpied.WordPress.HotPlug
+namespace PeachPied.WordPress.HotPlug
 {
     [Export(typeof(IWpPluginProvider))]
     sealed class Provider : IWpPluginProvider
     {
         IEnumerable<IWpPlugin>/*!!*/IWpPluginProvider.GetPlugins(IServiceProvider provider, string wpRootPath)
         {
-            //yield return new HotPlug(wpRootPath);
+            yield return new HotPlug(wpRootPath);
             yield break;
         }
     }
