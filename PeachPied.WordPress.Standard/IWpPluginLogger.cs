4f67209c2202d1dcc7afc60fe2eea5b62bbbab9e
Jakub Míšek <jmisek@gmail.com>
2021-04-03 23:33:40 +0200

    IWpPluginLogger

---
+++ PeachPied.WordPress.Standard/IWpPluginLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeachPied.WordPress.Standard
+{
+    /// <summary>
+    /// Interface representing a WordPress plugin.
+    /// </summary>
+    public interface IWpPluginLogger
+    {
+        /// <summary>
+        /// The log severity.
+        /// </summary>
+        public enum Severity
+        {
+            /// <summary>Informational message.</summary>
+            Message,
+            /// <summary>A warning.</summary>
+            Warning,
+            /// <summary>An error.</summary>
+            Error,
+        }
+
+        /// <summary>Logs the message into the app' console.</summary>
+        void Log(Severity severity, string message, Exception exception = null);
+    }
+}
