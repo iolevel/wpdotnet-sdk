c2cb1e8b0d286f99bb54e50cf804998a952be7b0
Jakub Míšek <jmisek@gmail.com>
2021-04-03 18:03:32 +0200

    hotplug class

---
+++ plugins/PeachPied.WordPress.HotPlug/FolderCompiler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeachPied.WordPress.HotPlug
+{
+    class FolderCompiler : IDisposable
+    {
+        public string RootPath { get; }
+
+        public string SubPath { get; }
+
+        public FolderCompiler(string rootPath, string subPath, string outputAssemblyName)
+        {
+            this.RootPath = rootPath;
+            this.SubPath = subPath;
+        }
+
+        public FolderCompiler Build(bool watch)
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ plugins/PeachPied.WordPress.HotPlug/HotPlug.cs
@@ -0,0 +1,33 @@
+﻿using PeachPied.WordPress.Standard;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeachPied.WordPress.HotPlug
+{
+    sealed class HotPlug : IWpPlugin
+    {
+        public string RootPath { get; }
+
+        static string WpPluginsSubPath => "wp-content/plugins";
+
+        static string WpThemesSubPath => "wp-content/themes";
+
+        static bool WatchForChanges => true;
+
+        readonly FolderCompiler _pluginsCompiler, _themesCompiler;
+
+        public HotPlug(string wpRootPath)
+        {
+            RootPath = wpRootPath;
+
+            _pluginsCompiler = new FolderCompiler(RootPath, WpPluginsSubPath, "wp-plugins").Build(WatchForChanges);
+            _themesCompiler = new FolderCompiler(RootPath, WpThemesSubPath, "wp-themes").Build(WatchForChanges);
+        }
+
+        void IWpPlugin.Configure(WpApp app)
+        {
+            // no hooks necessary
+        }
+    }
+}
