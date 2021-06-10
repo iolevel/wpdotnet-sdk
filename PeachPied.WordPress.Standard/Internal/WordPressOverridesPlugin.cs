8912eca7573ef929f9de5a3e4c6579022e81100e
Jakub Míšek <jmisek@gmail.com>
2021-06-10 18:36:14 +0200

    format code, wpdotnet footer

--- PeachPied.WordPress.Standard/Internal/WordPressOverridesPlugin.cs
+++ PeachPied.WordPress.Standard/Internal/WordPressOverridesPlugin.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Composition;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Runtime.Versioning;
@@ -30,7 +31,7 @@ namespace PeachPied.WordPress.Standard.Internal
                 System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription ??
                 Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
             var process = System.Diagnostics.Process.GetCurrentProcess();
-            
+
             output.Write($@"
 <table style=""border:0;width:100%;""><tbody>
 <tr>
@@ -38,7 +39,7 @@ namespace PeachPied.WordPress.Standard.Internal
     <td>
         <b>Your WordPress runs on {frameworkVersion}</b>
         <div>
-            <b title=""Memory allocated by the whole process."">Memory usage:</b> {process.WorkingSet64/1024/1024} MB<br/>
+            <b title=""Memory allocated by the whole process."">Memory usage:</b> {process.WorkingSet64 / 1024 / 1024} MB<br/>
             <b title=""CPU time consumed by the whole process."">CPU usage:</b> {process.TotalProcessorTime:c}<br/>
             <b>Project URL:</b> <a href=""{WpDotNetUrl}"" target=""_blank"">wpdotnet-sdk</a><br/>
         </div>
@@ -64,6 +65,16 @@ namespace PeachPied.WordPress.Standard.Internal
 
             // alter generator metadata
             app.AddFilter("get_the_generator_xhtml", new Func<string>(() => GeneratorHtml));
+
+            app.Footer(output =>
+            {
+                output.Write(@"
+<div style='position:fixed;text-align:center; width:128px; height:50px; bottom:0; right:0;padding:12px 0 12px 0; margin:0 auto; vertical-align: middle;background:#68b227;border-left:solid 6px rgba(255,255,255,.5);line-height:24px;'>
+    <a href='https://www.wpdotnet.com/#pricing' style='color:#fff;font-size:16px;font-style:tahoma,helvetica;padding:0; margin:0;'>Get Rid of Me</a>
+</div>
+");
+                //output.Write(@"<div style='margin:-55px 0 0 0;width:70px;height:70px;left:0;' class='foldtl'>&nbsp;</div>");
+            });
         }
     }
 }
