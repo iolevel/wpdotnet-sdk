64d4bda82354c28831dbd5cb58c087457a5ee8db
Jakub Míšek <jmisek@gmail.com>
2023-01-23 14:20:19 +0100

    updated RegisterPartialViewAsShortcode() with view model

--- PeachPied.WordPress.AspNetCore/WpAppExtension.cs
+++ PeachPied.WordPress.AspNetCore/WpAppExtension.cs
@@ -18,10 +18,23 @@ namespace PeachPied.WordPress.AspNetCore
     {
         /// <summary>
         /// Register shortcode that renders shared partial view (<c>.cshtml</c> file).
+        /// <br/>
+        /// Shortcode can be used in WordPress in form <c>[viewName]</c>.
         /// </summary>
-        public static void RegisterPartialViewAsShortcode(this WpApp app, string viewName, PhpValue viewModel = default(PhpValue))
+        /// <param name="app">WordPress request instance.</param>
+        /// <param name="viewName">Name of the shared partial view. Also name of the registered shortcode.</param>
+        /// <param name="viewModelFunc">Optional function that provides model for the view. The input parameter is a dictionary of shortcode arguments provided by WordPress.</param>
+        public static void RegisterPartialViewAsShortcode(this WpApp app,
+            string viewName,
+            Func<IDictionary<IntStringKey, PhpValue>, object?>? viewModelFunc = null)
         {
-            app.AddShortcode(viewName, new shortcode_handler((attrs, _, _) => app.Context.Partial(viewName)));
+            app.AddShortcode(viewName, new shortcode_handler((attrs, _, _) =>
+            {
+                //
+                object? viewModel = viewModelFunc?.Invoke(attrs);
+
+                return app.Context.Partial(viewName, viewModel);
+            }));
         }
     }
 }
